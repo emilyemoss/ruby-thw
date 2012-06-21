b53c7f5829b8193ea51048cea416bc3b34bdd0d1
Emily Moss <emilyemoss@gmail.com>
2012-06-21 17:55:50 -0400

    exercise 1

---
+++ ex1.rb
@@ -0,0 +1,17 @@
+# Learning Ruby the Hard Way 
+# Courtesy of @zedshaw 
+# Exercise 1: A Good First Program
+
+puts "Hello World!"
+puts "Hello again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+# Extra Credit
+
+puts "" # print a line
+puts "Another line." # add another line
+# put a hash in front of a line = COMMENT
